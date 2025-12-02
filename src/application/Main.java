b07a8563e7e11ea82c708ce65a65a82527ee9614
Mattia Nicolis <mattia.nicolis04@gmail.com>
2025-12-02 17:31:32 +0100

    aggiornato main

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,11 +3,6 @@ package application;
 import application.service.AdminService;
 import application.view.Navigator;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,23 +14,8 @@ public class Main extends Application {
 	
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/Login.fxml"));
-            Parent root = loader.load();
-
             Navigator.getInstance().setStage(primaryStage);
-            
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Software per pazienti diabetici");
-            primaryStage.show();
-			
-            Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-            primaryStage.setX(screenBounds.getMinX());
-            primaryStage.setY(screenBounds.getMinY());
-            primaryStage.setWidth(screenBounds.getWidth());
-            primaryStage.setHeight(screenBounds.getHeight());
-            primaryStage.setMaximized(true);
-			
+            Navigator.getInstance().switchToLogin(null);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
