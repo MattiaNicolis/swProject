9fdeab8d405572a7baf458d8ac902bdf7a3497d3
Mattia Nicolis <mattia.nicolis04@gmail.com>
2025-12-23 18:49:57 +0100

    nuovo main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,12 @@
 package application;
 
-import application.service.AdminService;
 import application.view.Navigator;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
-	public void init() throws Exception {
-		// Carico inizialmente solo gli utenti dal database per fare le liste pazienti e diabetologi
-		AdminService.loadAllUtenti();
-	}
+	public void init() throws Exception {}
 	
 	public void start(Stage primaryStage) {
 		try {
