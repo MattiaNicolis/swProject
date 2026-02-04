8fe13e4774fa028b43a4b8be61b38a0bdded70cb
Mattia Nicolis <mattia.nicolis04@gmail.com>
2026-02-04 16:57:43 +0100

    aggiunto nuovi metodi

--- src/application/service/AdminService.java
+++ src/application/service/AdminService.java
@@ -6,36 +6,40 @@ import java.util.List;
 import java.util.function.Function;
 
 import application.dao.impl.DatiDAO;
+import application.dao.impl.DiabetologoDAO;
 import application.dao.impl.GlicemiaDAO;
 import application.dao.impl.MailDAO;
 import application.dao.impl.PatologiaDAO;
+import application.dao.impl.PazienteDAO;
 import application.dao.impl.PesoDAO;
 import application.dao.impl.QuestionarioDAO;
 import application.dao.impl.TerapiaConcomitanteDAO;
 import application.dao.impl.TerapiaDAO;
-import application.dao.impl.UtenteDAO;
 import application.model.Dato;
+import application.model.Diabetologo;
 import application.model.Glicemia;
 import application.model.Mail;
 import application.model.Patologia;
+import application.model.Paziente;
 import application.model.Peso;
 import application.model.Questionario;
 import application.model.Terapia;
 import application.model.TerapiaConcomitante;
 import application.model.Utente;
+import application.model.UtenteInfo;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 
 public class AdminService {
-
-	// --- DORMATTAZIONE DATE E TEMPO ---	
+	
 	public static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	public static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-    // --- DAO ---
+    // DAO
     private static TerapiaDAO terapiaDAO = new TerapiaDAO();
-    private static UtenteDAO utenteDAO = new UtenteDAO();
+    private static PazienteDAO pazienteDAO = new PazienteDAO();
+	private static DiabetologoDAO diabetologoDAO = new DiabetologoDAO();
 	private static DatiDAO datiDAO = new DatiDAO();
 	private static PatologiaDAO patologiaDAO = new PatologiaDAO();
 	private static TerapiaConcomitanteDAO terapiaConcomitanteDAO = new TerapiaConcomitanteDAO();
@@ -44,10 +48,14 @@ public class AdminService {
 	private static QuestionarioDAO questDAO = new QuestionarioDAO();
 	private static PesoDAO pesoDAO = new PesoDAO();
 
-	// --- DAO SETTER PER TEST ---
-	public static void setUtenteDAO(UtenteDAO dao) {
-        utenteDAO = dao;
+	// DAO SETTER PER TEST
+	public static void setPazienteDAO(PazienteDAO dao) {
+        pazienteDAO = dao;
     }
+
+	public static void setDiabetologoDAO(DiabetologoDAO dao) {
+		diabetologoDAO = dao;
+	}
 
 	public static void setTerapiaDAO(TerapiaDAO dao) {
 		terapiaDAO = dao;
@@ -82,13 +90,17 @@ public class AdminService {
 	}
 
 	// ---------------------------------
-	// CERCA UTENTE PER LOGIN
-	public static Utente login(String cf, String password) {
-		return utenteDAO.login(cf, password);
+	// CERCA PAZIENTE PER LOGIN
+	public static Paziente loginPaziente(String cf, String password) {
+		return pazienteDAO.loginPaziente(cf, password);
 	}
 	// CREA LISTE PAZIENTI E DIABETOLOGI
-	public static List<Utente> getPeopleByRole(String role) {
-		return utenteDAO.getPeopleByRole(role);
+	public static List<Paziente> getPazienti() {
+		return pazienteDAO.getPazienti();
+	}
+	// CERCA DIABETOLOGO PER LOGIN
+	public static Diabetologo loginDiabetologo(String cf, String password) {
+		return diabetologoDAO.loginDiabetologo(cf, password);
 	}
 	
 	// -------------------------------------------
@@ -96,42 +108,34 @@ public class AdminService {
 	public static boolean creaTerapia(Terapia t) {
 		return terapiaDAO.creaTerapia(t);
 	}
-
 	// MODIFICA TERAPIA
 	public static boolean modificaTerapia(Terapia t) {
 		return terapiaDAO.modificaTerapia(t);
 	}
-	
 	// ELIMINA TERAPIA
 	public static boolean eliminaTerapia(Terapia t) {
 		return terapiaDAO.eliminaTerapia(t);
 	}
-
 	// NOTIFICA TERAPIA
 	public static boolean notificaTerapia(Terapia t) {
 		return terapiaDAO.notificaTerapia(t);
 	}
-
 	// CARICA TERAPIE DI UN PAZIENTE DAL DATABASE
 	public static List<Terapia> loadTerapieByPaziente(Utente paziente) {
 		return terapiaDAO.getTerapieByPaziente(paziente);
 	}
-
 	// CARICA NUMERO DI TERAPIE ATTIVE PER PAZIENTE IN UNA CERTA DATA
 	public static int loadTerapieAttiveByCfAndData(String cf, LocalDate data) {
 		return terapiaDAO.getNumeroTerapieAttive(cf, data);
 	}
-
 	// CARICA NUMERO DI TERAPIE SODDISFATTE PER PAZIENTE IN UNA CERTA DATA
 	public static int loadTerapieSoddisfatteByCfAndData(String cf, LocalDate data) {
 		return terapiaDAO.getTerapieSoddisfatte(cf, data);
 	}
-
 	// AGGIORNA NUMERO QUESTIONARI PER TERAPIA
 	public static boolean loadAggiornaNumQuestionari(Terapia t) {
 		return terapiaDAO.aggiornaNumQuestionari(t);
 	}
-
 	// GET TERAPIA CON ID
 	public static Terapia getTerapiaById(int id) {
 		return terapiaDAO.getTerapiaById(id);
@@ -142,12 +146,10 @@ public class AdminService {
 	public static List<Peso> loadPesoByCf(String cf) {
 		return pesoDAO.getPesoByCf(cf);
 	}
-
 	// AGGIORNA PESO
 	public static boolean aggiornaPeso(Peso p) {
 		return pesoDAO.aggiornaPeso(p);
 	}
-
 	// CREA PESO
 	public static boolean creaPeso(Peso p) {
 		return pesoDAO.creaPeso(p);
@@ -158,12 +160,10 @@ public class AdminService {
 	public static List<Dato> loadFattoriByPaziente(Utente paziente) {
 		return datiDAO.getDatiByPaziente(paziente, "fattori");
 	}
-	
 	// CREA FATTORE
 	public static boolean creaFattore(Dato fattore) {
 		return datiDAO.creaDato(fattore, "fattori");
 	}
-
 	// ELIMINA FATTORE
 	public static boolean eliminaFattore(Dato fattore) {
 		return datiDAO.eliminaDato(fattore, "fattori");
@@ -174,12 +174,10 @@ public class AdminService {
 	public static List<Dato> loadComorbiditàByPaziente(Utente paziente) {
 		return datiDAO.getDatiByPaziente(paziente, "comorbidità");
 	}
-	
 	// CREA COMORBIDITA'
 	public static boolean creaComorbidità(Dato comorbidità) {
 		return datiDAO.creaDato(comorbidità, "comorbidità");
 	}
-
 	// ELIMINA COMORBIDITA'
 	public static boolean eliminaComorbidità(Dato comorbidità) {
 		return datiDAO.eliminaDato(comorbidità, "comorbidità");
@@ -190,12 +188,10 @@ public class AdminService {
 	public static List<Dato> loadAllergieByPaziente(Utente paziente) {
 		return datiDAO.getDatiByPaziente(paziente, "allergie");
 	}
-
 	// CREA ALLERGIA
 	public static boolean creaAllergia(Dato allergia) {
 		return datiDAO.creaDato(allergia, "allergie");
 	}
-
 	// ELIMINA ALLERGIA
 	public static boolean eliminaAllergia(Dato allergia) {
 		return datiDAO.eliminaDato(allergia, "allergie");
@@ -206,12 +202,10 @@ public class AdminService {
 	public static List<Glicemia> loadGlicemiaByPaziente(Utente paziente) {
 		return glicemiaDAO.getGlicemiaByPaziente(paziente);
 	}
-
 	// CREA GLICEMIA
 	public static boolean creaGlicemia(Glicemia g) {
 		return glicemiaDAO.creaGlicemia(g);
 	}
-
 	// CARICA TUTTE LE GLICEMIE DAL DATABASE
 	public static List<Glicemia> loadAllGlicemia() {
 		return glicemiaDAO.getAllGlicemia();
@@ -222,33 +216,35 @@ public class AdminService {
 	public static List<Mail> loadMailRicevute(Utente utente) {
 		return mailDAO.getMailRicevute(utente);
 	}
-
 	// CARICA MAIL INVIATE DAL DATABASE
 	public static List<Mail> loadMailInviate(Utente utente) {
 		return mailDAO.getMailInviate(utente);
 	}
-
 	// SEGNA MAIL COME LETTA
 	public static boolean vediMail(Mail m) {
 		return mailDAO.vediMail(m);
 	}
-
 	// SCRIVI MAIL
 	public static boolean scriviMail(Mail m) {
 		return mailDAO.scriviMail(m);
 	}
-
+	// RITORNA MAIL DIABETOLOGO RIFERITO A PAZIENTE
+	public static String getMailDiabetologoRif(String cf) {
+		return mailDAO.getMailDiabetologoRif(cf);
+	}
+	// UTENTE INFO
+	public static List<UtenteInfo> getUtenteInfo(String role) {
+		return mailDAO.getUtenteInfo(role);
+	}
 	// ---------------------------------------------------------------------
 	// CARICA PATOLOGIE DAL DATABASE
 	public static List<Patologia> loadPatologieByPaziente(Utente paziente) {
 		return patologiaDAO.getPatologieByPaziente(paziente);
 	}
-
 	// CREA PATOLOGIA
 	public static boolean creaPatologia(Patologia p) {
 		return patologiaDAO.creaPatologia(p);
 	}
-
 	// ELIMINA PATOLOGIA
 	public static boolean eliminaPatologia(Patologia p) {
 		return patologiaDAO.eliminaPatologia(p);
@@ -259,12 +255,10 @@ public class AdminService {
 	public static List<TerapiaConcomitante> loadTerapieConcomitantiByPaziente(Utente paziente) {
 		return terapiaConcomitanteDAO.getTerapieConcomitantiByPaziente(paziente);
 	}
-
 	// CREA TERAPIA CONCOMITANTE
 	public static boolean creaTerapiaConcomitante(TerapiaConcomitante tc) {
 		return terapiaConcomitanteDAO.creaTerapiaConcomitante(tc);
 	}
-
 	// ELIMINA TERAPIA CONCOMITANTE
 	public static boolean eliminaTerapiaConcomitante(TerapiaConcomitante tc) {
 		return terapiaConcomitanteDAO.eliminaTerapiaConcomitante(tc);
@@ -275,22 +269,18 @@ public class AdminService {
 	public static List<Questionario> loadQuestionariByPaziente(Utente paziente) {
 		return questDAO.getQuestionariByPaziente(paziente);
 	}
-
 	// CARICA TUTTI I QUESTIONARI NON CONFORMI DAL DATABASE
 	public static List<Questionario> loadQuestionariNonConformi() {
 		return questDAO.getQuestionariNonConformi();
 	}
-
 	// SEGNA QUESTIONARIO COME CONTROLLATO
 	public static boolean segnaComeControllato(Questionario q) {
 		return questDAO.segnaComeControllato(q);
 	}
-
 	// CREA QUESTIONARIO
 	public static boolean creaQuestionario(Questionario q) {
 		return questDAO.creaQuestionario(q);
 	}
-
 	// ESISTE QUESTIONARIO OGGI
 	public static boolean esisteQuestionarioOggi(int terapiaId) {
 		return questDAO.esisteQuestionarioOggi(terapiaId);
@@ -300,19 +290,36 @@ public class AdminService {
 	// METODI DI ACCESSO RAPIDO
 	//---------------------------------------------	
 	
-	// RITORNA UTENTE
-	public static Utente getUtenteByCf(List<Utente> utenti, String cf) {
-		return utenti.stream()
-				.filter(utente -> utente.getCf().equals(cf))
+	// RITORNA PAZIENTE DA CF
+	public static Paziente getPazienteByCf(List<Paziente> pazienti, String cf) {
+		return pazienti.stream()
+				.filter(paziente -> paziente.getCf().equals(cf))
 				.findFirst()
 				.orElse(null);
 	}
 	
-	// RITORNA NOME UTENTE
-	public static String getNomeUtenteByCf(List<Utente> utenti, String cf) {
-		Utente u = getUtenteByCf(utenti, cf);
-		if(u != null) {
-			return u.getNomeCognome();
+	// RITORNA NOME PAZIENTE DA CF
+	public static String getNomePazienteByCf(List<Paziente> pazienti, String cf) {
+		Paziente p = getPazienteByCf(pazienti, cf);
+		if(p != null) {
+			return p.getNome() + " " + p.getCognome();
+		}
+		return null;
+	}
+
+	// RITORNA DIABETOLOGO DA CF
+	public static Diabetologo getDiabetologoByCf(List<Diabetologo> diabetologi, String cf) {
+		return diabetologi.stream()
+				.filter(diabetologo -> diabetologo.getCf().equals(cf))
+				.findFirst()
+				.orElse(null);
+	}
+	
+	// RITORNA NOME DIABETOLOGO DA CF
+	public static String getNomeDiabetologoByCf(String cf) {
+		String nomeCognome = diabetologoDAO.getNomeCognomeDiabetologoByCf(cf);
+		if(nomeCognome != null) {
+			return nomeCognome;
 		}
 		return null;
 	}
@@ -360,4 +367,5 @@ public class AdminService {
 			}
 		});
 	}
+
 }
